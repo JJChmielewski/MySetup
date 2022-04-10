fb1d68f694790207db63261cdd80a71ae2310bc5
Chmielu <j.j.chmielewski@wp.pl>
2022-04-10 02:55:24 +0200

    added debloat script

--- my-setup.bat
+++ my-setup.bat
@@ -1,9 +1,12 @@
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\Chmielu\MySetup\Wallpaper.jpg
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
+powershell -Command "&{iwr -useb https://git.io/JJ8R4 | iex}"
+
 color 0B
 
 winget import C:\Users\Chmielu\MySetup\winget.txt
+
 
 setup /download config.xml
 setup /configure config.xml
