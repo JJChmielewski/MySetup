113e92149ce91a667a875c0f81a1fae0aa68b84d
Chmielu <j.j.chmielewski@wp.pl>
2022-04-10 03:13:06 +0200

    Changed file paths so that they are not stupid

--- my-setup.bat
+++ my-setup.bat
@@ -1,11 +1,11 @@
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\Chmielu\MySetup\Wallpaper.jpg
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %~dp0\Wallpaper.jpg
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
 powershell -Command "&{iwr -useb https://git.io/JJ8R4 | iex}"
 
 color 0B
 
-winget import C:\Users\Chmielu\MySetup\winget.txt
+winget import %~dp0\winget.txt
 
 
 setup /download config.xml
